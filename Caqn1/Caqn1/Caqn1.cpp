d7a6d98b85d290dbe480072ccebaff209f7cdd92
Kerreg <95027797+Kerreg@users.noreply.github.com>
2021-11-25 16:40:45 +0300

    первый коммит

---
+++ Caqn1/Caqn1/Caqn1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string s;
+
+    ifstream fin ("C:\\Users\\студент\\Desktop\\Сайт.html");
+    string line;
+    string result = "";
+
+    if (fin.is_open())
+    {
+        while (getline(fin, line))
+        {
+            if (line == "<h1></h1>") {
+                result += "<h1>Сайт чата</h1>";
+            }
+            else {
+                result += line;
+            }
+            result += "\n";
+        }
+    }
+    
+    fin.close();
+
+    ofstream fout("C:\\Users\\студент\\Desktop\\Сайт2.html");
+    fout << result;
+    fout.close();
+}
---
+++ Source1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <ifstream>
+using namespace std;
+
+int main() {
+	
+	ifsteam file;
+	file.open("C:\Users\студент\Desktop\Сайт.txt");
+
+}
